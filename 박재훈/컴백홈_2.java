e042c060112cd8bcf754805b3790aafcdd972a1e
jhp336 <67370317+jhp336@users.noreply.github.com>
2023-02-24 14:26:55 +0900

    solve2 : boj Q1189 컴백홈
    visited 배열 없는 풀이

---
+++ 박재훈/컴백홈_2.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int R, C, K, ans;
+    static char[][] map;
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+  
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        R = Integer.parseInt(input[0]);
+        C = Integer.parseInt(input[1]);
+        K = Integer.parseInt(input[2]);
+      
+        map = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            char[] temp = br.readLine().toCharArray();
+            System.arraycopy(temp, 0, map[i], 0, C);
+        }
+      
+      
+        map[R-1][0] = 'T';
+      //왼쪽 아래에서 출발. 시작점도 거리에 포함되므로 cnt값 1 설정
+        dfs(1, R-1,0);
+        System.out.println(ans);
+    }
+    static void dfs(int cnt, int r, int c){
+        if(cnt > K){
+           //이미 K거리 이상 갔으면 탈락
+            return;
+        }
+        if(cnt == K) {
+          //집 도착, 거리 K면 가짓수++
+            if(r == 0 && c == C-1){
+                ans++;
+            }
+            return;
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if(nr >= 0 && nr < R && nc >= 0 && nc < C
+                     && map[nr][nc] == '.'){
+              //장애물 아니면 dfs 탐색, 지나온 곳은 장애물로 표시하여 다시 못가도록함
+                map[nr][nc] = 'T';
+                dfs(cnt + 1, nr, nc);
+              //해당 점 탐색결과 원하는 K가 아니었다면
+              //장애물로 해놓은 걸 다시 풀어줌
+                map[nr][nc] = '.';
+            }
+        }
+    }
+}

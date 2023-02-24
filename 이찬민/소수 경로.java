106b79fbc7e123d86ae016b55adb266ac46f3f50
LCM <62368629+lecocococo@users.noreply.github.com>
2023-02-24 11:14:18 +0900

    refactor: 배열생성을 줄이고 코드의 중복제거

--- 이찬민/소수 경로.java
+++ 이찬민/소수 경로.java
@@ -4,22 +4,23 @@ import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
-public class Main {
+public class Main_1963 {
     static int testCase;
     static int f;
     static int l;
-    static int answer;
+    static int answer = -1;
     static int[] first;
     static int[] last;
     static boolean[] prime;
     static boolean[] visited;
+    static int[] d = {1000, 100, 10, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         prime = new boolean[10000];
         testCase = Integer.parseInt(br.readLine());
         prime();
-        
+
         for (int i = 0; i < testCase; i++) {
             String[] temp = br.readLine().split(" ");
             f = Integer.parseInt(temp[0]);
@@ -39,57 +40,75 @@ public class Main {
             visited = new boolean[10000];
 
             bfs(0);
-            System.out.println(answer);
+            if (answer == -1) {
+                System.out.println("Impossible");
+            }
+            if (answer != -1) {
+                System.out.println(answer);
+            }
+
         }
     }
 
     static void bfs(int cnt){
         Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{first[0], first[1],first[2],first[3], cnt});
+        q.add(new int[]{f, cnt});
         visited[f] = true;
         while (!q.isEmpty()) {
             int[] temp = q.poll();
-            int count = temp[4];
-            if (toInt(new int[]{temp[0], temp[1], temp[2], temp[3]}) == l) {
+            int num = temp[0];
+            int count = temp[1];
+
+            if (num == l) {
                 answer = count;
                 return;
             }
 
+            int[] nums = toIntArr(num);
+
             for (int i = 0; i < 10; i++) {
-                
-                // 중복 코드 메서도 바꾸자
-                int tVal = toInt(new int[]{i, temp[1], temp[2], temp[3]});
-                //첫번째자리가 0인경우는 빼주도록하자
-                if (i !=0 && prime[tVal] == false && !visited[tVal]) {
-                    q.add(new int[]{i, temp[1], temp[2], temp[3], count + 1});
-                    visited[tVal] = true;
+                for (int j = 0; j < 4; j++) {
+                    int tVal = makeNum(j, nums, i);
+                    //첫번째자리가 0인경우는 빼주도록하자
+                    if (tVal > 1000) {
+                        addPrime(q,tVal,count);
+                    }
                 }
-
-                tVal = toInt(new int[]{temp[0], i, temp[2], temp[3]});
-                if (prime[tVal] == false && !visited[tVal]){
-                    q.add(new int[]{temp[0], i, temp[2], temp[3], count + 1});
-                    visited[tVal] = true;
-                }
-                tVal = toInt(new int[]{temp[0], temp[1], i, temp[3]});
-                if (prime[tVal] == false && !visited[tVal]) {
-                    q.add(new int[]{temp[0], temp[1], i, temp[3], count + 1});
-                    visited[tVal] = true;
-                }
-
-                tVal = toInt(new int[]{temp[0], temp[1], temp[2], i});
-                if (prime[tVal] == false && !visited[tVal]) {
-                    q.add(new int[]{temp[0], temp[1], temp[2], i, count + 1});
-                    visited[tVal] = true;
-                }
-
             }
         }
     }
 
-    static int toInt(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(arr[0]).append(arr[1]).append(arr[2]).append(arr[3]);
-        return Integer.parseInt(sb.toString());
+    static int[] toIntArr(int num) {
+        int[] nums = new int[4];
+        for (int i = 0; i < 4; i++) {
+            nums[i] = num/d[i];
+            num %= d[i];
+        }
+        return nums;
+    }
+
+    static int makeNum(int pos, int[] nums, int i) {
+        int num = 0;
+        if (pos == 0) {
+            num = i * d[0] + nums[1] * d[1] + nums[2] * d[2] + nums[3];
+        }
+        if (pos == 1) {
+            num = nums[0] * d[0] + i * d[1] + nums[2] * d[2] + nums[3];
+        }
+        if (pos == 2) {
+            num = nums[0] * d[0] + nums[1] * d[1] + i * d[2] + nums[3];
+        }
+        if (pos == 3) {
+            num = nums[0] * d[0] + nums[1] * d[1] + nums[2] * d[2] + i;
+        }
+        return num;
+    }
+
+    static void addPrime(Queue<int[]> q, int tVal, int count) {
+        if (prime[tVal] == false && !visited[tVal]) {
+            q.add(new int[]{tVal, count + 1});
+            visited[tVal] = true;
+        }
     }
 
     static void prime() {
